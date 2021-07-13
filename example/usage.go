3c92a78dd116c62061f303d860c342b9940f6df0
Hejun Deng <denghejun@live.cn>
2021-07-13 14:59:28 +0800

    update vault login examples

--- example/usage.go
+++ example/usage.go
@@ -22,7 +22,7 @@ func vaultLoginWithinAwsEcsContainerAutomaticallyExample() *api.Client {
 	return vaultClient
 }
 
-func vaultLoginByAwsRoleArn() *api.Client {
+func vaultLoginByAwsRoleArnExample() *api.Client {
 	_ = os.Setenv("AWS_ACCESS_KEY_ID", "AKKKAFYN9K3AUY74SNY39") // the parent aws credentials to call aws api to assume the specific role
 	_ = os.Setenv("AWS_SECRET_ACCESS_KEY", "ZTL9288NXdymfhxcISMOCU+AsYy9O3RsiDptABm8")
 	vaultClient := goawsvault.LoginByAwsRoleArn("https://vault.mycompany.net", "arn:aws:iam::468785217309:role/my-application-service-dev", "eu-west-1")
